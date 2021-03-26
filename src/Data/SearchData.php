22850f9c73da2f55ed266e2bc0c642a1863a90bd
oussema <oussema.abderrahmen@esprit.tn>
2021-03-26 17:21:10 +0100

    third commit

---
+++ src/Data/SearchData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Data;
+
+use App\Entity\Category;
+
+class SearchData
+{
+    /**
+     * @var integer
+     */
+
+    public $page= '1';
+
+    /**
+     * @var string
+     */
+
+    public $q = '';
+
+    /**
+     * @var Category[]
+     */
+    public $categories = [] ;
+}
